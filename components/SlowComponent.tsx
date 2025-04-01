635b267efe7bc808a51b708a6c5b7a6523ec8acf
Matthieu Mordrel <matthieumordrel@gmail.com>
2025-04-01 22:23:43 +0200

    minor updates

--- components/SlowComponent.tsx
+++ components/SlowComponent.tsx
@@ -1,13 +1,18 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function SlowComponent({ data }: { data: any }) {
+  console.log('SlowComponent streaming with data:', data.timestamp)
   const [state, setState] = useState(data)
 
+  useEffect(() => {
+    console.log('SlowComponent hydrated with timestamp:', state.timestamp)
+  }, [])
+
   const incrementYear = () => {
-    const date = new Date(state)
+    const date = new Date(state.timestamp)
     date.setFullYear(date.getFullYear() + 1)
     setState(date.toISOString())
   }
@@ -16,7 +21,7 @@ function SlowComponent({ data }: { data: any }) {
     <div className='p-4 bg-blue-100 rounded-lg'>
       <h2 className='text-xl font-semibold'>Slow Component</h2>
       <p>This component took 2 seconds to load!</p>
-      <p>Loaded data: {state.title}</p>
+      <p>Loaded data: {data.title}</p>
       <p>Timestamp: {state.timestamp}</p>
       <Button onClick={incrementYear} className='mt-2'>
         Increment Year
--- components/SlowerComponent.tsx
+++ components/SlowerComponent.tsx
@@ -1,10 +1,15 @@
 'use client'
 
 import { Button } from '@/components/ui/button'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 function SlowerComponent({ data }: { data: any }) {
+  console.log('SlowerComponent streaming with data:', data.timestamp)
   const [timestamp, setTimestamp] = useState(data.timestamp)
+
+  useEffect(() => {
+    console.log('SlowerComponent hydrated with timestamp:', timestamp)
+  }, [])
 
   const incrementYear = () => {
     const date = new Date(timestamp)
