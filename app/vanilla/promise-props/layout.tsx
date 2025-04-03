5bf2fe65a5879aba740d0eaf3d0f435e128f16b2
Matthieu Mordrel <matthieumordrel@gmail.com>
2025-04-03 21:34:54 +0200

    test using force dynamic

--- app/vanilla/promise-props/layout.tsx
+++ app/vanilla/promise-props/layout.tsx
@@ -1,9 +1,9 @@
 import { fetchSlowData, fetchSlowerData } from '@/lib/data'
-import { connection } from 'next/dist/server/request/connection'
 import { DataProvider } from './DataProvider'
 
+export const dynamic = 'force-dynamic'
+
 export default async function PromisePropsLayout({ children }: { children: React.ReactNode }) {
-  await connection()
   // Start fetching data at the layout level
   const slowPromise = fetchSlowData()
   const slowerPromise = fetchSlowerData()
