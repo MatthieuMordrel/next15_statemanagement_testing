9989378c3ad47cfd0c9a3b8e2acc20bd55b9b9a2
MatthieuMordrel <matthieumordrel@gmail.com>
2025-04-08 14:14:23 +0200

    removed headers

--- app/api/dummyDataSlow/route.ts
+++ app/api/dummyDataSlow/route.ts
@@ -1,12 +1,6 @@
-import { headers } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
-  //Interestingly, headers are empty when streaming the query from the server to the client
-  const headerlist = await headers()
-  headerlist.forEach((value, key) => {
-    console.log(key, value)
-  })
   await new Promise(resolve => setTimeout(resolve, 2000))
   return NextResponse.json({
     id: 'Slow',
